ee936d3b9d709e26a0a01ded308241b2049a9a6a
Jay Davis <davis.jaym@gmail.com>
2016-04-13 16:58:29 +0100

    Working project page

--- src/components/ui/projects/project.js
+++ src/components/ui/projects/project.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class Project extends Component {
   constructor() {
     super();
   }
 
+  renderProject(projects, id) {
+    return projects.filter(project => {
+      return project.id === id;
+    })
+    .map(item => {
+      return (
+        <div key={item.id}
+             className="panel">
+          { item.title }
+        </div>
+      );
+    });
+  }
+
   render() {
-    console.log(this.context);
+    let id = parseInt(this.props.params.id, 10)
+      , projects = this.context.projectList.projects;
+
     return(
       <div>
-        Project
+        { this.renderProject(projects, id) }
       </div>
     );
   }
 }
+
+Project.contextTypes = {
+  projectList: PropTypes.object.isRequired
+};
 
 export default Project;
